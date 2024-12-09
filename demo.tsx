7b1a3d5edf9cb8f5ebb7f820d593a082a5fe67c3
Morgan Ney <morganney@gmail.com>
2024-12-08 21:07:52 -0600

    feat: add props to demo. (#7)

--- demo.tsx
+++ demo.tsx
@@ -1,32 +1,133 @@
-import React, { useState, useEffect } from 'react'
+import React, {
+  type ChangeEvent,
+  useEffect,
+  useReducer,
+  useCallback,
+} from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { StockTicker } from './src/stockTicker.js'
+import { StockTicker, type StockTickerProps } from './src/stockTicker.js'
 import { usePrevious } from './src/hooks.js'
 import { formatter } from './src/utils.js'
 
+const reducer = (state: StockTickerProps, action: StockTickerProps) => {
+  return { ...state, ...action }
+}
 const getRandomBetween = (min: number, max: number) => {
   return Math.random() * (max - min) + min
 }
 const Demo = () => {
-  const [price, setPrice] = useState(999.85)
-  const prevPrice = usePrevious(price)
+  const [state, dispatch] = useReducer(reducer, {
+    price: 999.85,
+    fontSize: '36px',
+    color: '#000000',
+    upColor: '#008000',
+    downColor: '#FF0000',
+    duration: 350,
+    timingFunction: 'ease-out',
+  })
+  const prevPrice = usePrevious(state.price)
+  const handler = useCallback(
+    (evt: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+      const { name, value } = evt.target
+
+      dispatch({ ...state, [name]: value })
+    },
+    [state]
+  )
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setPrice((prevPrice) => prevPrice + getRandomBetween(-0.1, 0.15))
+      dispatch({ price: prevPrice + getRandomBetween(-0.1, 0.15) })
     }, 3_500)
 
     return () => clearInterval(interval)
-  }, [])
+  }, [prevPrice])
 
   return (
-    <div>
-      <StockTicker fontSize="32px" price={price} />
-      <p>Previous price: {formatter.format(prevPrice)}</p>
-    </div>
+    <section>
+      <h1>
+        <a href="https://www.npmjs.com/package/react-stock-ticker">
+          <code>react-stock-ticker</code>
+        </a>
+      </h1>
+      <div>
+        <StockTicker {...state} />
+        <p>Previous: {formatter.format(prevPrice)}</p>
+      </div>
+      <form>
+        <fieldset>
+          <legend>Props</legend>
+          <div>
+            <label>
+              <code>color</code>
+              <input
+                type="color"
+                name="color"
+                value={state.color}
+                onChange={handler}
+              />
+            </label>
+            <label>
+              <code>upColor</code>
+              <input
+                type="color"
+                name="upColor"
+                value={state.upColor}
+                onChange={handler}
+              />
+            </label>
+            <label>
+              <code>downColor</code>
+              <input
+                type="color"
+                name="downColor"
+                value={state.downColor}
+                onChange={handler}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              <code>fontSize</code>
+              <input
+                type="text"
+                name="fontSize"
+                value={state.fontSize}
+                onChange={handler}
+              />
+            </label>
+            <label>
+              <code>duration</code>
+              <input
+                type="number"
+                name="duration"
+                value={state.duration}
+                onChange={handler}
+                step={25}
+              />
+            </label>
+            <label>
+              <code>timingFunction</code>
+              <select
+                name="timingFunction"
+                value={state.timingFunction}
+                onChange={handler}>
+                <option value="ease">ease</option>
+                <option value="ease-in">ease-in</option>
+                <option value="ease-out">ease-out</option>
+                <option value="ease-in-out">ease-in-out</option>
+                <option value="linear">linear</option>
+                <option value="step-start">step-start</option>
+                <option value="step-end">step-end</option>
+              </select>
+            </label>
+          </div>
+        </fieldset>
+      </form>
+    </section>
   )
 }
-const root = createRoot(document.getElementById('root') as HTMLDivElement)
+const root = createRoot(document.querySelector('main')!)
 
 root.render(<Demo />)
